e102684fcfe3cc8aa597a64d87f700dca804d9a7
Dishagoel1804 <147609519+Dishagoel1804@users.noreply.github.com>
2025-08-17 01:19:14 +0530

    Time: 3 ms (13.4%), Space: 41.1 MB (5.76%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximum69Number (int num) {
+        String s=""+num;
+        s=s.replaceFirst("6","9");
+        /*for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='6')
+            {
+            s=s.replace(s.charAt(i),'9');
+            break;
+            }
+        }*/
+        return Integer.valueOf(s);
+    }
+}
